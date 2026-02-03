f7b57b5dbde4bf42a5c45a80aef607a115aee98d
Vikuuu <adivik672@gmail.com>
2026-02-03 22:43:44 +0530

    feat(app): Testing with fyne

--- main.go
+++ main.go
@@ -1,49 +1,26 @@
 package main
 
 import (
-	"errors"
-	"fmt"
-	"os/exec"
-	"path/filepath"
-	"time"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
 )
 
+func makeUI() (*widget.Label, *widget.Entry) {
+	out := widget.NewLabel("Hello world!")
+	in := widget.NewEntry()
+
+	in.OnChanged = func(content string) {
+		out.SetText("Hello " + content + "!")
+	}
+
+	return out, in
+}
+
 func main() {
-	now := time.Now()
-	invoiceFileName := fmt.Sprintf("invoice-%d-%02d-%02d.pdf", now.Year(), now.Month(), now.Day())
-	pwd, err := filepath.Abs(".")
-	if err != nil {
-		panic(err)
-	}
+	a := app.New()
+	w := a.NewWindow("Invoice Generator")
 
-	invoiceFilePath := filepath.Join(pwd, "invoices", invoiceFileName)
-
-	paths, err := filepath.Glob("data/*.typ")
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("%+q", paths)
-
-	path, err := filepath.Abs("data/main.typ")
-	if err != nil {
-		panic(err)
-	}
-	typstCmd := exec.Command("typst", "compile", path, invoiceFilePath)
-
-	out, err := typstCmd.Output()
-	if err != nil {
-		var execErr *exec.Error
-		var exitErr *exec.ExitError
-		switch {
-		case errors.As(err, &execErr):
-			fmt.Println("Failed executing: ", err)
-		case errors.As(err, &exitErr):
-			exitCode := exitErr.ExitCode()
-			fmt.Println("command rc = ", exitCode)
-			fmt.Println(string(exitErr.Stderr))
-		default:
-			panic(err)
-		}
-	}
-	fmt.Println(string(out))
+	w.SetContent(container.NewVBox(makeUI()))
+	w.ShowAndRun()
 }
