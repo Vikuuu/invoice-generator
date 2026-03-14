38bf5292e629a34965d9c05bdd98adaf2b9e4397
Vikuuu <adivik672@gmail.com>
2026-03-14 16:40:04 +0530

    feat(schema): Add invoice table

--- internal/migrations/001_create_tables.sql
+++ internal/migrations/001_create_tables.sql
@@ -32,11 +32,57 @@ CREATE TABLE IF NOT EXISTS item (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL,
     hsn INTEGER NOT NULL,
-    price INTEGER NOT NULL,
+    price DECIMAL(19, 2) NOT NULL,
     gst INTEGER NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS invoice_item (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    fk_invoice INTEGER,
+    fk_item INTEGER,
+    qty INTEGER,
+
+    FOREIGN KEY (fk_invoice)
+        REFERENCES invoice (id)
+            ON DELETE CASCADE,
+
+    FOREIGN KEY (fk_item)
+        REFERENCES item (id)
+            ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS invoice (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    invoice_number INTEGER NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    fk_from_company INTEGER NOT NULL,
+    fk_to_company INTEGER NOT NULL,
+    fk_payment_detail INTEGER NOT NULL,
+    igst DECIMAL(19, 2) NOT NULL,
+    TOTAL DECIMAL(19, 2) NOT NULL,
+    fk_shipping_address integer NOT NULL,
+
+    FOREIGN KEY (fk_from_company)
+        REFERENCES company (id)
+            ON DELETE CASCADE,
+
+    FOREIGN KEY (fk_to_company)
+        REFERENCES company (id)
+            ON DELETE CASCADE,
+
+    FOREIGN KEY (fk_payment_detail)
+        REFERENCES payment_detail (id)
+            ON DELETE CASCADE,
+
+    FOREIGN KEY (fk_shipping_address)
+        REFERENCES shipping_address (id)
+            ON DELETE CASCADE
+);
+
 -- +goose Down
+DROP TABLE IF EXISTS invoice;
+DROP TABLE IF EXISTS invoice_item;
 DROP TABLE IF EXISTS item;
 DROP TABLE IF EXISTS shipping_address;
 DROP TABLE IF EXISTS payment_detail;
